8b8dd976e1a492ccbb3e4672570a4fe349b69eb6
Dylan Wettlaufer <dylanwett11@gmail.com>
2025-06-11 21:31:45 -0400

    change to group ui

--- frontend/src/features/Groups/pages/Group.jsx
+++ frontend/src/features/Groups/pages/Group.jsx
@@ -11,9 +11,26 @@ const Group = () => {
     const [group, setGroup] = useState({});
     const [members, setMembers] = useState([]);
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isMembersSubmitting, setIsMembersSubmitting] = useState(false);
 
     const params = useParams();
     const id = params.id;
+    const [transactions, setTransactions] = useState([]);
+
+    const dummyExpenses = [
+        {
+            name: "Groceries",
+            amount: 100,
+            date: "2025-06-11",
+            category: "Food",
+            group_id: id,
+            user_id: 1,
+            paid_by: 1,
+            description: "Groceries for the week",
+            is_settled: false,
+            split: 2
+        }
+    ]
 
     useEffect(() => {
         const fetchGroup = async () => {
@@ -33,13 +50,13 @@ const Group = () => {
     useEffect(() => {
         const fetchMembers = async () => {
             try {
-                setIsSubmitting(true);
+                setIsMembersSubmitting(true);
                 const response = await getGroupMembers(id);
                 setMembers(response);
             } catch (error) {
                 console.error('Error fetching members:', error);
             } finally {
-                setIsSubmitting(false);
+                setIsMembersSubmitting(false);
             }
         };
         fetchMembers();
@@ -48,10 +65,80 @@ const Group = () => {
     return (
         <div>
             <Header />
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">   
-                
-                <div className="mb-8 flex flex-col sm:flex-row justify-between gap-10">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
 
+                <div className="mb-8 flex flex-col sm:flex-row justify-between gap-10 bg-neutral-100 rounded-xl p-4 shadow-md">
+                    <div className='flex flex-col gap-2'>
+                        <h1 className="text-3xl font-semibold text-black">{group.name}</h1>
+                        <p className='text-zinc-600 text-sm'>{group.description}</p>
+                        <div className='flex flex-row items-center gap-2'>
+                            <Calendar className="w-4 h-4 text-zinc-600" />
+                            <p className='text-zinc-800 text-md'>Created on {new Date(group.created_at).toLocaleDateString(undefined, {
+                                    year: 'numeric',
+                                    month: 'long',
+                                    day: 'numeric',
+                                })}</p>
+                        </div>
+                        
+                    </div>
+                    <div className='flex flex-row items-center gap-2'>
+                        
+                        <div className='flex flex-row items-center gap-2 bg-neutral-200 p-4 rounded-xl'>
+                            <div className='rounded-lg bg-sky-200 p-2'>
+                                <DollarSign className="w-6 h-6 text-sky-600" />
+                            </div>
+                            <div>
+                                <p className='text-zinc-600 text-sm'>Total Expenses</p>
+                                <p className='text-zinc-800 font-semibold text-md'>$0</p>
+                            </div>
+                        </div>
+
+                        <div className='flex flex-row items-center gap-2 bg-neutral-200 p-4 rounded-xl'>
+                            {trendingUp ? (
+                                <div className='rounded-lg bg-emerald-200 p-2'>
+                                    <TrendingUp className="w-6 h-6 text-emerald-600" />
+                                </div>
+                            ) : (
+                                <div className='rounded-lg bg-red-200 p-2'>
+                                    <TrendingDown className="w-6 h-6 text-red-600" />
+                                </div>
+                            )}
+                            {trendingUp ? (
+                                <div>
+                                    <p className='text-zinc-600 text-sm'>Your Balance</p>
+                                    <p className='text-green-600 font-semibold text-md'>+$0</p>
+                                </div>
+                            ) : (
+                                <div>
+                                    <p className='text-zinc-600 text-sm'>Your Balance</p>
+                                    <p className='text-red-600 font-semibold text-md'>-$0</p>
+                                </div>
+                            )}
+                        </div>
+
+                        <div className='flex flex-row items-center gap-2 bg-neutral-200 p-4 rounded-xl'>
+                            <div className='rounded-lg bg-orange-200 p-2'>
+                                <Users className="w-6 h-6 text-orange-600" />
+                            </div>
+                            <div>
+                                <p className='text-zinc-600 text-sm'>Members</p>
+                                <p className='text-zinc-800 font-semibold text-md'>{members.length}</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    
+                </div>
+
+                <div className="mb-8 flex flex-col sm:flex-row justify-between gap-10">
+                    {isSubmitting ? (
+                        <div className="flex items-center justify-center py-12 w-full h-full bg-neutral-100">
+                            <div className="flex items-center space-x-3">
+                                <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-blue-600"></div>
+                                <p className="text-black">Loading group...</p>
+                            </div>
+                        </div>
+                    ) : (
                     <div className='flex flex-col bg-neutral-100 rounded-xl p-4 shadow-md gap-4 w-full h-full'>
                         <h1 className="text-3xl font-semibold text-black">{group.name}</h1>
                         <p className='text-zinc-600 text-sm pb-6'>{group.description}</p>
@@ -85,8 +172,7 @@ const Group = () => {
                                     <p className='text-red-600 font-semibold text-md'>-$0</p>
                                 </div>
                             )}
-                            
-                        </div>
+                        </div> 
                         <div className='flex flex-row items-center gap-2'>
                             <div className='rounded-lg bg-stone-200 p-2'>
                                 <Calendar className="w-6 h-6 text-stone-600" />
@@ -100,9 +186,19 @@ const Group = () => {
                                 })}</p>
                             </div>
                         </div>
-                        
-                    </div>
+                    </div> 
+                )}
 
+                    {/* Members */}
+
+                    {isSubmitting ? (
+                        <div className="flex items-center justify-center py-12 w-full h-full bg-neutral-100">
+                            <div className="flex items-center space-x-3">
+                                <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-blue-600"></div>
+                                <p className="text-black">Loading members...</p>
+                            </div>
+                        </div>
+                    ) : (
                     <div className='flex flex-col bg-neutral-100 rounded-xl p-4 shadow-md gap-2 w-full h-full'>
                         <div className="flex flex-row items-center justify-between gap-2 border-b border-zinc-200 pb-2">
                             <h1 className="text-lg font-semibold text-zinc-800">Members</h1>
@@ -110,7 +206,7 @@ const Group = () => {
                                 <UserRoundPlus className="w-6 h-6 text-zinc-600" />
                             </button>
                         </div>
-
+                    
                         <div className='flex flex-col gap-2'>
                             {members.map((member) => (
                                 <div className='flex flex-row items-center gap-2 border-b border-zinc-200 pb-2'>
@@ -124,9 +220,18 @@ const Group = () => {
                                 </div>
                             ))}
                         </div>
+                    </div>
+                    )}
+                </div>
+
+                {/* Transactions */}
+                <div className='flex flex-col bg-neutral-100 rounded-xl p-4 shadow-md gap-2 w-full h-full'>
+                    <h1 className='text-lg font-semibold text-zinc-800'>Recent Expenses</h1>
+                    <div className='flex flex-col gap-2'>
                         
                     </div>
-                </div>
+                </div>            
+
             </div>
         </div>
     );
